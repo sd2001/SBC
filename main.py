1fb07e539f84f5b68da9403771700a5c775fb215
sd2001 <im.swarnabha2001@gmail.com>
2021-06-19 15:02:32 +0530

    🎉 🔒️ Added the main file(calling classes)

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from solution import *
 
 # Calling and importing all the Class methods
     
-obj = Get_Blocks('./mempool.csv')
-txid, fee, wt, parents = obj.column_list()   
-res = obj.fee_per_weight()
+if __name__ == '__main__':    
+	obj = Get_Blocks('./mempool.csv')
+	txid, fee, wt, parents = obj.column_list()   
+	res = obj.fee_per_weight()
 
-txid, fee, wt, parents, res = obj.sorted()
-obj.get_init()
+	txid, fee, wt, parents, res = obj.sorted()
+	obj.get_init()
 
-net_wt, net_fee, ids = obj.get_ids()
-obj.write_to_csv()
-print(net_wt, net_fee)
+	net_wt, net_fee, ids = obj.get_ids()
+	obj.write_to_csv()
+	print(net_wt, net_fee)
